010573aad5adc2c1b6ba68dc96b5a6b2c5e670cf
Nicolas Mariano <nicmariano10@gmail.com>
2022-05-28 22:21:33 -0300

    reset pagina e empedir mensagens vazias

--- js/script.js
+++ js/script.js
@@ -7,13 +7,15 @@ function perguntarnome(){
         name: nome
       }
     const promessa = axios.post("https://mock-api.bootcamp.respondeai.com.br/api/v2/uol/participants", dados)
+    // colocar msg de carregamento
     promessa.then(buscarmensagem)
       promessa.catch(nomeinvalido)
 }
 
 function buscarmensagem(){
-    setInterval(carregarMSG_servidor, 3000)
-    setInterval(manterCONECTADO, 5000)
+    // tirar mensagem de carregamento
+    setInterval(carregarMSG_servidor, 2000)
+    setInterval(manterCONECTADO, 4000)
 }
 
 function nomeinvalido(){
@@ -55,7 +57,7 @@ function renderizar(resposta){
                 <strong>para</strong>
                 <strong>${novo.to}</strong>
                
-                <span>${novo.text}</span>
+                <h3>${novo.text}</h3>
              </li>`
         }
         else if(novo.to === nome || novo.from === nome || (novo.type === "private-message" && novo.to === "todos")  ){
@@ -65,11 +67,11 @@ function renderizar(resposta){
                <strong>${novo.from}</strong>
                <span>${novo.to}</span>
                <strong>somente </strong>
-               <span>${novo.text}</span>
+               <h3>${novo.text}</h3>
             </li>`
         }
     }
-
+    rolarchataofinal()
 }
 
 function manterCONECTADO(){
@@ -77,24 +79,36 @@ function manterCONECTADO(){
         name: nome
       }
     const promessa = axios.post("https://mock-api.bootcamp.respondeai.com.br/api/v2/uol/status", dados)
+    promessa.then(buscarmensagem)
 }
 
 function enviarmensagem(){
-    const mensagem = document.querySelector(".digitado").value
-    mensagem.focus = ""
-    const dados = {
-        from: nome,
-        to: "todos",
-        text: mensagem,
-        type: "message" // ou "private_message" para o bônus
+    const mensagem = document.querySelector(".digitado")
+    input = mensagem.value
+    if(mensagem != ""){
+        const dados = {
+            from: nome,
+            to: "todos",
+            text: input,
+            type: "message" // ou "private_message" para o bônus
+        }
+
+        const promessa = axios.post("https://mock-api.bootcamp.respondeai.com.br/api/v2/uol/messages", dados)
+        promessa.then(carregarMSG_servidor);
+        promessa.catch(atualizaPg)
+        mensagem.value = "";
+        
     }
+}
 
-    const promessa = axios.post("https://mock-api.bootcamp.respondeai.com.br/api/v2/uol/messages", dados)
-    console.log(mensagem)
-    onfocus="this.value='';"
+function atualizaPg(){
+    alert("vc foi desconectado")
+    location. reload();
+}
 
-    carregarMSG_servidor()
-
+function rolarchataofinal(){
+    const ultimamsg = document.querySelector(".lista li:last-child")
+    ultimamsg.scrollIntoView()
 }
 
 
